ebe781fa4b981ac5aebf396236a8efd1b4a63f05
thibault-manse <thibault.manse@laplateforme.io>
2025-11-12 12:08:26 +0100

    update : add asset file and can print text on screen

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <SFML/Window.hpp>
 #include <SFML/Graphics.hpp>
+#include <iostream>
 #include <optional>
 
 int main()
@@ -9,7 +10,6 @@ int main()
     printf("Blabla\n");
     while (window.isOpen())
     {
-        printf("Bonjour\n");
         while (const std::optional event = window.pollEvent())
         {
             if (event->is<sf::Event::Closed>())
@@ -20,6 +20,23 @@ int main()
 
         window.clear(sf::Color::Black);
         // Ici tu peux dessiner des formes, sprites, etc.
+        sf::Font font;
+        if (!font.openFromFile("asset/font/arial.ttf"))
+        {
+            std::cerr << "Échec de chargement de la police: asset/font/arial.ttf\n";
+            return 1;
+        }
+
+        sf::Text playButton(font, "Jouer");
+        playButton.setCharacterSize(40);
+        playButton.setPosition(playButton.getPosition() + sf::Vector2f{0.f, 200.f});
+
+        sf::Text quitButton(font, "Quitter");
+        quitButton.setCharacterSize(40);
+        quitButton.setPosition(quitButton.getPosition() + sf::Vector2f{0.f, 300.f});
+
+        window.draw(playButton);
+        window.draw(quitButton);
         window.display();
     }
     return 0;
